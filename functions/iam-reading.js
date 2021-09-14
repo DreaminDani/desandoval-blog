a670d5a67f2bbdff79de1337ec53bcac49891fe6
Daniel Sandoval <danielericsandoval@gmail.com>
2021-09-14 05:33:44 -0700

    empty response for reading endpoint

--- functions/iam-reading.js
+++ functions/iam-reading.js
@@ -1,12 +1,10 @@
-const RSSParser = require("rss-parser");
-
+// Todo replace with raindrop
+// Todo remove RSS parser as a dependency
 exports.handler = async function (event, context, callback) {
   try {
-    const rss = new RSSParser();
-    const response = await rss.parseURL('http://wallabag.desandoval.net/daniel/Uie7r5pfgWbD7eH/unread.xml')
     return  {
       statusCode: 200,
-      body: JSON.stringify(response.items)
+      body: JSON.stringify({})
     }
   } catch {
     return  {
